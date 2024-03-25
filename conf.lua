f7cde44d0406bfbf6b2419fd977639b07466d0cd
eduvf <155389249+eduvf@users.noreply.github.com>
2024-03-25 18:07:49 +0100

    update conf

--- conf.lua
+++ conf.lua
@@ -1,6 +1,9 @@
 function love.conf(t)
-  t.window.title = "olive"
+  t.version = '11.3'
+  t.window.title = 'olive'
 
-  t.window.width = 1280
-  t.window.height = 720
+  t.window.width = 240
+  t.window.height = 136
+
+  t.window.resizable = true
 end
