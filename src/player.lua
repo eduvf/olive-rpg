589028e43e343a26e10f9be8fb8c9533555a9aa0
eduvf <155389249+eduvf@users.noreply.github.com>
2024-05-12 23:22:44 +0200

    use spr function

--- src/player.lua
+++ src/player.lua
@@ -10,17 +10,6 @@ player = {
 }
 
 function player.load()
-  player.image = love.graphics.newImage('gfx/player.png')
-  player.quad = {
-    love.graphics.newQuad(0, 0, 8, 8, player.image),
-    love.graphics.newQuad(8, 0, 8, 8, player.image),
-    love.graphics.newQuad(0, 8, 8, 8, player.image),
-    love.graphics.newQuad(8, 8, 8, 8, player.image),
-    love.graphics.newQuad(0, 16, 8, 8, player.image),
-    love.graphics.newQuad(8, 16, 8, 8, player.image),
-    love.graphics.newQuad(0, 24, 8, 8, player.image),
-    love.graphics.newQuad(8, 24, 8, 8, player.image),
-  }
 end
 
 function player.update(dt)
@@ -65,24 +54,16 @@ function player.update(dt)
   if input.lt then player.flip = true end
   if input.rt then player.flip = false end
 
-  if love.keyboard.isScancodeDown('1') then player.anim = 1 end
-  if love.keyboard.isScancodeDown('2') then player.anim = 2 end
-  if love.keyboard.isScancodeDown('3') then player.anim = 3 end
-  if love.keyboard.isScancodeDown('4') then player.anim = 4 end
+  if love.keyboard.isScancodeDown('1') then player.anim = PLAYER1 end
+  if love.keyboard.isScancodeDown('2') then player.anim = PLAYER2 end
+  if love.keyboard.isScancodeDown('3') then player.anim = PLAYER3 end
 end
 
 function player.draw()
-  local frame = math.floor(game.time * 2) % 2 + (player.anim * 2 - 1)
+  local frame = math.floor(game.time * 2) % 2
   
   local x = player.px * game.scale
   local y = player.py * game.scale
-  local sx, sy = game.scale, game.scale
-  local ox, oy = 0, 0
-  if player.flip then
-    x = x + game.scale
-    sx = sx * -1
-    ox = game.scale
-  end
 
   love.graphics.setColor(0, 0, 0, 0.5)
   local rect_scale = 8 * game.scale
@@ -90,5 +71,5 @@ function player.draw()
   local rect_y = player.y * rect_scale
   love.graphics.rectangle('fill', rect_x, rect_y, rect_scale, rect_scale)
   love.graphics.setColor(1, 1, 1)
-  love.graphics.draw(player.image, player.quad[frame], x, y, 0, sx, sy, ox, oy)
+  spr(player.anim + frame, x, y, game.scale, player.flip)
 end
