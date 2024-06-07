9664941d681b55cfc7eeedd5c54ce18879b78327
eduvf <155389249+eduvf@users.noreply.github.com>
2024-06-07 11:46:54 +0200

    simple map

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 function love.load()
   game = {
     time = 0,
-    scale = 6
+    scale = 6,
+    map = {
+      canvas = nil
+    }
   }
 
   love.graphics.setDefaultFilter('nearest')
   require('src/lib')
+
+  map()
 end
 
 function love.update(dt)
@@ -13,5 +18,6 @@ function love.update(dt)
 end
 
 function love.draw()
-  sprite(math.floor(game.time+1), 0, 0, true)
+  love.graphics.draw(game.map.canvas, 0, 0, 0, game.scale)
+  sprite(math.floor(game.time+1), 0, 0, game.scale)
 end
--- src/lib.lua
+++ src/lib.lua
@@ -8,10 +8,24 @@ for y = 0, height - 1, 8 do
   end
 end
 
-function sprite(n, x, y, flip)
-  local sx, ox = game.scale, 0
+function sprite(n, x, y, scale, flip)
+  local scale = scale or 1
+  local sx, ox = scale, 0
   if flip then
-    sx, ox = -game.scale, 8
+    sx, ox = -scale, 8
   end
-  love.graphics.draw(image, quad[n], x, y, 0, sx, game.scale, ox)
+  love.graphics.draw(image, quad[n], x, y, 0, sx, scale, ox)
+end
+
+function map()
+  local width, height = 16, 9
+  game.map.canvas = love.graphics.newCanvas(width * 8, height * 8)
+
+  love.graphics.setCanvas(game.map.canvas)
+  for y = 0, height do
+    for x = 0, width do
+      sprite(5, x * 8, y * 8)
+    end
+  end
+  love.graphics.setCanvas()
 end
