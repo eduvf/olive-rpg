5b9e47a1fb2107b71495abd7d270f8e6cdb0d161
eduvf <>
2024-06-14 15:31:05 +0200

    change resolution

--- conf.lua
+++ conf.lua
@@ -2,8 +2,8 @@ function love.conf(t)
   t.version = '11.3'
   t.window.title = 'olea'
 
-  t.window.width = 640
-  t.window.height = 360
+  t.window.width = 1280
+  t.window.height = 720
 
   t.window.resizable = true
 end
